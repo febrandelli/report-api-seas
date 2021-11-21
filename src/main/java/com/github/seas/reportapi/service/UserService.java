fbc4f96a67087109c84d112848af3b957c41b7cb
febrandelli <fe.brandelli@gmail.com>
2021-11-21 10:54:14 -0300

    Implementado endpoint para que possa ser resetado a senha do usuario

--- src/main/java/com/github/seas/reportapi/service/UserService.java
+++ src/main/java/com/github/seas/reportapi/service/UserService.java
@@ -2,12 +2,16 @@ package com.github.seas.reportapi.service;
 
 import com.github.seas.reportapi.controller.form.UserCreateForm;
 import com.github.seas.reportapi.controller.form.UserUpdateForm;
+import com.github.seas.reportapi.controller.response.ResetPasswordResponse;
 import com.github.seas.reportapi.domain.Perfil;
 import com.github.seas.reportapi.domain.Usuario;
 import com.github.seas.reportapi.exception.NotFoundException;
 import com.github.seas.reportapi.repository.PerfilRepository;
 import com.github.seas.reportapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +21,8 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
+    private final JavaMailSender mailSender;
     private final UsuarioRepository usuarioRepository;
     private final PerfilRepository perfilRepository;
     private final PasswordEncoder passwordEncoder;
@@ -64,5 +70,50 @@ public class UserService {
 
     public void deleteUser(Long id){
         usuarioRepository.deleteById(id);
+    }
+
+    public ResponseEntity<ResetPasswordResponse> resetPassword (String email) throws NotFoundException {
+        String newPassword = generatePassword(10);
+        Usuario user =  usuarioRepository.findByEmail(email).orElseThrow(() -> new NotFoundException("Email não existe"));
+        sendEmailWithNewPassword(newPassword, email);
+        user.setSenha(passwordEncoder.encode(user.getSenha()));
+        return ResponseEntity.ok(new ResetPasswordResponse(Boolean.TRUE));
+
+    }
+
+    private void sendEmailWithNewPassword(String password, String email) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Sua senha foi alterada, sua nova senha será: ");
+        builder.append(password).append("\n");
+        builder.append("Lembre se de altera-la no seu proximo acesso.");
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setText(builder.toString());
+        mailMessage.setTo(email);
+        mailMessage.setFrom("noreply@seas.com.br");
+
+        try {
+            mailSender.send(mailMessage);
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+    }
+
+    private String generatePassword(int n) {
+
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                    + "0123456789"
+                                    + "abcdefghijklmnopqrstuvxyz";
+
+        StringBuilder sb = new StringBuilder(n);
+
+        for (int i = 0; i < n; i++) {
+            int index
+                    = (int)(AlphaNumericString.length()
+                    * Math.random());
+            sb.append(AlphaNumericString
+                    .charAt(index));
+        }
+        return sb.toString();
     }
 }
