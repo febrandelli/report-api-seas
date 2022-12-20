e2e4546cab5919c258016b4decdcefc004317e17
febrandelli <fe.brandelli@gmail.com>
2022-12-20 13:56:17 -0300

    feat: create new endpoints to optionals

---
+++ src/main/java/com/github/seas/reportapi/controller/dto/SearchOptionalDto.java
@@ -0,0 +1,11 @@
+package com.github.seas.reportapi.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SearchOptionalDto {
+		private String id;
+		private String nomeclatura;
+}
---
+++ src/main/java/com/github/seas/reportapi/domain/enums/SatisfactionEnum.java
@@ -0,0 +1,7 @@
+package com.github.seas.reportapi.domain.enums;
+
+public enum SatisfactionEnum {
+		BOM,
+		REGULAR,
+		RUIM;
+}
